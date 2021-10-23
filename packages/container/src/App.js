4650fa3a996cdf6f7b42c1f950fa7418bc3d9610
Cristian Palacios <cristianpalacios@tuhabi.mx>
2021-10-23 16:32:23 -0500

    Se introdujo marketing en container

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,8 +1,12 @@
 import React from "react";
-import { mount } from "marketing/MarketingApp";
-
-console.log(mount);
+import MarketingApp from "./components/MarketingApp";
 
 export default () => {
-  return <h1>Hi there container</h1>;
+  return (
+    <div>
+      <h1>Hi there, I am the container!</h1>
+      <hr />
+      <MarketingApp />;
+    </div>
+  );
 };
